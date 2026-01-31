cbf7517017f68257ff300183228154efa3931587
Luca Mike Motta <lucamotta4646@gmail.com>
2026-01-31 11:07:57 +0100

    Update Albero_BST.cpp

--- Strutture dati/Albero_BST.cpp
+++ Strutture dati/Albero_BST.cpp
@@ -5,18 +5,16 @@ using namespace std;
 class BST_Node
 {
 public:
-    BST_Node(int d): data(d), left(nullptr), right(nullptr), parent(nullptr) {}
+    BST_Node(int d) : data(d), left(nullptr), right(nullptr), parent(nullptr) {}
 
-    BST_Node* getLeft() const {return left;}
-    BST_Node* getRight() const {return right;}
-    BST_Node* getParent() const {return parent;}
-    int getData() const {return data;}
+    int getData() const { return data; }
+    BST_Node* getLeft() const { return left; }
+    BST_Node* getRight() const { return right; }
+    BST_Node* getParent() const { return parent; }
 
-    void setLeft(BST_Node* l) {left = l;}
-    void setRight(BST_Node* r) {right = r;}
-    void setParent(BST_Node* p) {parent = p;}
-
-
+    void setLeft(BST_Node* l) { left = l; }
+    void setRight(BST_Node* r) { right = r; }
+    void setParent(BST_Node* p) { parent = p; }
 
 private:
     int data;
@@ -30,299 +28,209 @@ class BST
 public:
     BST() : root(nullptr) {}
 
-    bool insert(int val);
+    ~BST()
+    {
+        clear(root);
+        root = nullptr;
+    }
 
-    BST_Node* search(int val);
-    BST_Node* min();
-    BST_Node* max();
-    BST_Node* successivo(BST_Node* node);
-    
-    void trapianta(BST_Node* u, BST_Node*v);
-    bool remove(int d);
-    
-    void print_pre(ostream& os) const {return print_pre(os,root);}
-    void print_in(ostream& os) const {return print_in(os,root);}
-    void print_post(ostream& os) const {return print_post(os,root);}
+    void insert(int val)
+    {
+        BST_Node* x = root;
+        BST_Node* y = nullptr;
 
-    void print_pre(ostream& os,BST_Node* b) const;
-    void print_in(ostream& os,BST_Node* b) const;
-    void print_post(ostream& os,BST_Node* b) const;
+        while (x != nullptr)
+        {
+            y = x;
+            if (val == x->getData())
+                return;
+            else if (val < x->getData())
+                x = x->getLeft();
+            else
+                x = x->getRight();
+        }
 
+        BST_Node* newnode = new BST_Node(val);
+        newnode->setParent(y);
+
+        if (root == nullptr)
+            root = newnode;
+        else if (val < y->getData())
+            y->setLeft(newnode);
+        else
+            y->setRight(newnode);
+    }
+
+    BST_Node* search(int val)
+    {
+        return search(root, val);
+    }
+
+    BST_Node* min()
+    {
+        return min(root);
+    }
+
+    BST_Node* successivo(int val)
+    {
+        BST_Node* node = search(val);
+        if (!node) return nullptr;
+
+        if (node->getRight())
+            return min(node->getRight());
+
+        BST_Node* p = node->getParent();
+        while (p != nullptr && node == p->getRight())
+        {
+            node = p;
+            p = p->getParent();
+        }
+        return p;
+    }
+
+    bool remove(int val)
+    {
+        BST_Node* node = search(val);
+        if (!node) return false;
+
+        if (node->getLeft() == nullptr)
+        {
+            trapianta(node, node->getRight());
+        }
+        else if (node->getRight() == nullptr)
+        {
+            trapianta(node, node->getLeft());
+        }
+        else
+        {
+            BST_Node* next = min(node->getRight());
+
+            if (next->getParent() != node)
+            {
+                trapianta(next, next->getRight());
+                next->setRight(node->getRight());
+                next->getRight()->setParent(next);
+            }
+
+            trapianta(node, next);
+            next->setLeft(node->getLeft());
+            next->getLeft()->setParent(next);
+        }
+
+        delete node;
+        return true;
+    }
+
+    void print(ostream& os) const
+    {
+        os << "=== PRE ORDER ===" << endl;
+        pre_order_print(root,os);
+        os << "\n=== IN ORDER ===" << endl;
+        in_order_print(root,os);
+        os << "\n=== POST ORDER ===" << endl;
+        post_order_print(root,os);
+        os << endl;
+    }
 
 private:
     BST_Node* root;
 
-    BST_Node* search(BST_Node* node, int val);
-    BST_Node* min(BST_Node* node);
-    BST_Node* max(BST_Node* node);
-    
+    void trapianta(BST_Node* u, BST_Node* v)
+    {
+        if (u->getParent() == nullptr)
+            root = v;
+        else if (u == u->getParent()->getLeft())
+            u->getParent()->setLeft(v);
+        else
+            u->getParent()->setRight(v);
 
+        if (v != nullptr)
+            v->setParent(u->getParent());
+    }
 
+    BST_Node* search(BST_Node* node, int val)
+    {
+        while (node != nullptr)
+        {
+            if (node->getData() == val)
+                return node;
+            else if (val < node->getData())
+                node = node->getLeft();
+            else
+                node = node->getRight();
+        }
+        return nullptr;
+    }
+
+    BST_Node* min(BST_Node* node)
+    {
+        if (!node) return nullptr;
+        while (node->getLeft() != nullptr)
+        {
+            node = node->getLeft();
+        }
+        return node;
+    }
+
+    void pre_order_print(BST_Node* x, ostream& os) const
+    {
+        if (!x) return;
+        os << x->getData() << " ";
+        pre_order_print(x->getLeft(),os);
+        pre_order_print(x->getRight(),os);
+    }
+
+    void in_order_print(BST_Node* x, ostream& os) const
+    {
+        if (!x) return;
+        in_order_print(x->getLeft(), os);
+        os << x->getData() << " ";
+        in_order_print(x->getRight(), os);
+    }
+
+    void post_order_print(BST_Node* x,ostream& os) const
+    {
+        if (!x) return;
+        post_order_print(x->getLeft(), os);
+        post_order_print(x->getRight(), os);
+        os << x->getData() << " ";
+    }
+
+    void clear(BST_Node* x)
+    {
+        if (!x) return;
+        clear(x->getLeft());
+        clear(x->getRight());
+        delete x;
+    }
 };
 
-
-bool BST::insert(int val)
+ostream& operator << (ostream& os, const BST& b) 
 {
-    BST_Node* x = root;                   //Ci permette di scorrere l'albero
-    BST_Node* y = nullptr;                // Tiene traccia del parent del nodo da inserire
-
-    while(x != nullptr)                   // Viene eseguito dopo aver inserito un nodo
-    {
-        y = x;
-
-        if(val == x->getData())             //Cosi evitiano duplicati 
-            return false;
-
-        else if(val < x->getData())         // Il valore è minore, vai a sinistra
-            x = x->getLeft();               
-
-        else
-            x = x->getRight();              // Il valore è maggiore, vai a destra
-    }
-
-    BST_Node* node = new BST_Node(val);     //Creazione nuovo nodo con il suo dato
-    node->setParent(y);                     // Collega il nuovo nodo al suo parent
-
-    if(y == nullptr)                        // Albero vuoto, nuovo nodo diventa root
-        root = node;                        
-
-    else if(val < y->getData())             
-        y->setLeft(node);                   // Inserisci come figlio sinistro
-
-    else
-        y->setRight(node);                  // Inserisci come figlio destro
-
-    return true;
+    b.print(os);
+    return os;
 }
-
-BST_Node* BST::search(int val)
-{
-    return search(root, val);
-}
-
-BST_Node* BST::search(BST_Node* node, int val)
-{
-
-    if(node == nullptr || node->getData() == val)
-        return node;
-
-    if(val < node->getData())
-        return search(node->getLeft(), val);
-    else
-        return search(node->getRight(), val);
-}
-
-BST_Node* BST::min()
-{
-    return min(root);
-}
-
-
-BST_Node* BST::min(BST_Node* node)
-{
-    if(node == nullptr || node->getLeft() == nullptr)
-    return node;
-
-    return min(node->getLeft());
-}
-
-BST_Node* BST::max()
-{
-    return max(root);
-}
-
-
-BST_Node* BST::max(BST_Node* node)
-{
-    if(node == nullptr || node->getRight() == nullptr)
-    return node;
-
-    return max(node->getRight());
-}
-
-BST_Node* BST::successivo(BST_Node* node)
-{
-    if(node == nullptr )
-        return nullptr;
-
-    if(node->getRight())
-        return min(node->getRight());
-
-    BST_Node* genitore = node->getParent();
-    while(genitore && node == genitore->getRight())
-    {
-        node = genitore;
-        genitore = node->getParent();
-    }
-
-    return genitore;
-}
-
-void BST::trapianta(BST_Node* u, BST_Node*v)
-{
-    if(u->getParent() == nullptr)
-    {
-        root = v;
-    }
-    else if(u == u->getParent()->getLeft())
-    {
-        u->getParent()->setLeft(v);
-    }
-    else
-    {
-        u->getParent()->setRight(v);
-    }
-
-    if(v != nullptr)
-    {
-        v->setParent(u->getParent());
-    }
-}
-
-bool BST::remove(int d)
-{
-    BST_Node* p = search(d);
-
-    if(p == nullptr)
-    {
-        cout << "Nessun nodo contenente il valore : " << d << endl;
-        return false;
-    }
-
-    // Caso 1: Foglia
-    if(!p->getLeft() && !p->getRight())
-    {
-       trapianta(p,nullptr);
-       delete p;
-       return true;
-    }
-
-    // Caso 2: Solo figlio destro
-    if(!p->getLeft())
-    {
-        trapianta(p,p->getRight());
-        delete p;
-        return true;
-    }
-
-    // Caso 2: Solo figlio sinistro
-    if(!p->getRight())
-    {
-        trapianta(p,p->getLeft());
-        delete p;
-        return true;
-    }
-
-    // Caso 3: Entrambi figli
-    BST_Node* next = successivo(p);
-
-    if(next->getParent() != p)
-    {
-        trapianta(next,next->getRight());
-        next->setRight(p->getRight());
-        next->getRight()->setParent(next);
-    }
-    
-    trapianta(p,next);
-
-    if(p->getLeft())
-    {
-        next->setLeft(p->getLeft());
-        next->getLeft()->setParent(next);
-    }
-
-    delete p;
-    return true;
-    
-}
-
-void BST::print_pre(ostream& os, BST_Node* b) const
-{
-    if(b==nullptr) return;
-
-    os << b->getData() << " ";
-    print_pre(os,b->getLeft());
-    print_pre(os,b->getRight());
-}
-
-void BST::print_in(ostream& os, BST_Node* b) const
-{
-    if(b==nullptr) return;
-
-    print_in(os,b->getLeft());
-    os << b->getData() << " ";
-    print_in(os,b->getRight());
-}
-
-void BST::print_post(ostream& os, BST_Node* b) const
-{
-    if(b==nullptr) return;
-
-    print_post(os,b->getLeft());
-    print_post(os,b->getRight());
-    os << b->getData() << " ";
-}
-
-ostream& operator << (ostream& os, const BST& b)
-{
-   os << "=== PRE ORDER ===" << endl;
-   b.print_pre(os);
-   os << endl;
-
-   os << "=== IN ORDER ===" << endl;
-   b.print_in(os);
-   os << endl;
-
-   os << "=== POST ORDER ===" << endl;
-   b.print_post(os);
-   os << endl;
-
-   return os;
-}
-
 
 int main()
 {
-    BST b;
-    b.insert(40);
-    b.insert(30);
-    b.insert(50);
-    b.insert(20);
-    b.insert(25);
-    b.insert(35);
-    b.insert(45);
-    b.insert(60);
-    b.insert(70);
-    b.insert(65);
-    b.insert(55);
-    b.insert(75);
+    BST albero;
 
-    cout << b << endl;
+    albero.insert(50);
+    albero.insert(30);
+    albero.insert(10);
+    albero.insert(40);
+    albero.insert(70);
+    albero.insert(20);
+    albero.insert(60);
+    albero.insert(80);
 
-    BST_Node* s = b.search(50);
-    BST_Node* s1 = b.search(80);
+    cout << "=== PRE ALBERO ===" << endl;
+    cout << albero;
 
-    if(s)
-        cout << "NODO TROVATO: "<< s->getData() << endl;
-    else
-        cout << "NODO NON TROVATO" << endl;
-
-    if(s1)
-        cout << "NODO TROVATO: "<< s1->getData() << endl;
-    else
-        cout << "NODO NON TROVATO" << endl;
-
-    BST_Node* min = b.min();
-    cout << "NODO CON IL VALORE MINIMO DELL'ALBERO: " << min->getData() << endl;
-
-    BST_Node* max = b.max();
-    cout << "NODO CON IL VALORE MASSIMO DELL'ALBERO: " << max->getData() << endl;
-
-    b.remove(45); // caso 1 foglia;
-    b.remove(70); // caso 2 figlio (dx)
-    b.remove(65); // caso 3 entrambi figli
-
-    cout << b << endl;
+    albero.remove(50);
+    
+    cout << "\n=== POST ALBERO ===" << endl;
+    cout << albero;
 
     return 0;
 }
